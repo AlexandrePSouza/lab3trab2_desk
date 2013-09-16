cbce8d2eba26c546283e717328c5bc49458dcf08
finger75 <alexandre.finger@esales.com.br>
2013-09-15 21:51:31 -0300

    Classe main

--- src/br/com/senac/nutricare/Main.java
+++ src/br/com/senac/nutricare/Main.java
@@ -17,7 +17,6 @@ public class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class);
     
     public static void main(String[] args) {
-        
         logger.info("Application starter");
     }
 }
